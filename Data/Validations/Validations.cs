babd397dfd036b02957ed7feedcb8ed04bd2374c
Brian Blankenship <c0dezer019@zohomail.com>
2022-06-08 11:34:54 -0500

    Renamed file to include all custom validations.

---
+++ Data/Validations/Validations.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace D20.Data.Forms.Validations
+{
+  class Username : ValidationAttribute
+  {
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+      if (!value.ToString().ToLower().Contains("admin") ||
+          !value.ToString().ToLower().Contains("moderator") ||
+          !value.ToString().ToLower().Contains("staff"))
+      {
+        return null;
+      }
+
+      return new ValidationResult("We prefer our users not to pretend they are staff. Please choose a different username.");
+    }
+  }
+}
