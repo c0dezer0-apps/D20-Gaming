4a69378b104f0d08a612b39b710334400a76a076
Brian Blankenship <c0dezer019@zohomail.com>
2022-06-08 11:28:18 -0500

    Formatting files.

--- Data/Entities/Character.cs
+++ Data/Entities/Character.cs
@@ -2,140 +2,140 @@
 
 namespace D20.Data.Entities.Characters
 {
-    interface ICharacter
-    {
-        string FirstName { get; set; }
-        string MiddleName { get; set; }
-        string LastName { get; set; }
-        string PhysicalDescription { get; set; }
-        string Mother { get; set; }
-        string Father { get; set; }
-        string Children { get; set; }
-        string Siblings { get; set; }
-        string PersonalityAndTraits { get; set; }
-        string WeaknessesAndStrengths { get; set; }
-        string Biography { get; set; }
-        Employment[] EmploymentHistory { get; set; }
-        CharacterAward[] Awards { get; set; }
+  interface ICharacter
+  {
+    string FirstName { get; set; }
+    string MiddleName { get; set; }
+    string LastName { get; set; }
+    string PhysicalDescription { get; set; }
+    string Mother { get; set; }
+    string Father { get; set; }
+    string Children { get; set; }
+    string Siblings { get; set; }
+    string PersonalityAndTraits { get; set; }
+    string WeaknessesAndStrengths { get; set; }
+    string Biography { get; set; }
+    Employment[] EmploymentHistory { get; set; }
+    CharacterAward[] Awards { get; set; }
 
-    }
-    /// <summary>
-    /// Intended to be a very flexible model. Game Masters decide which values to use.
-    /// Validation is intended to be done on on the frontend.
-    /// </summary>
-    public class Character : ICharacter
-    {
-        public string FirstName { get; set; }
+  }
+  /// <summary>
+  /// Intended to be a very flexible model. Game Masters decide which values to use.
+  /// Validation is intended to be done on on the frontend.
+  /// </summary>
+  public class Character : ICharacter
+  {
+    public string FirstName { get; set; }
 
-        public string MiddleName { get; set; }
+    public string MiddleName { get; set; }
 
-        public string LastName { get; set; }
+    public string LastName { get; set; }
 
-        public string PhysicalDescription { get; set; }
+    public string PhysicalDescription { get; set; }
 
-        public string Mother { get; set; }
+    public string Mother { get; set; }
 
-        public string Father { get; set; }
+    public string Father { get; set; }
 
-        public string Children { get; set; }
+    public string Children { get; set; }
 
-        public string Siblings { get; set; }
+    public string Siblings { get; set; }
 
-        public string PersonalityAndTraits { get; set; }
+    public string PersonalityAndTraits { get; set; }
 
-        public string WeaknessesAndStrengths { get; set; }
+    public string WeaknessesAndStrengths { get; set; }
 
-        public string Biography { get; set; }
+    public string Biography { get; set; }
 
-        public Employment[] EmploymentHistory { get; set; }
+    public Employment[] EmploymentHistory { get; set; }
 
-        public CharacterAward[] Awards { get; set; }
+    public CharacterAward[] Awards { get; set; }
 
-        public string GeneralHealth { get; set; }
+    public string GeneralHealth { get; set; }
 
-        public string Injuries { get; set; }
+    public string Injuries { get; set; }
 
-        public string Surgeries { get; set; }
+    public string Surgeries { get; set; }
 
-        public string Illnesses { get; set; }
+    public string Illnesses { get; set; }
 
-        public string MentalHealth { get; set; }
+    public string MentalHealth { get; set; }
 
-        public string Modifications { get; set; }
+    public string Modifications { get; set; }
 
-        public string Archetype { get; set; }
+    public string Archetype { get; set; }
 
-        public string PrimaryPosition { get; set; }
+    public string PrimaryPosition { get; set; }
 
-        public string SecondaryPosition { get; set; }
+    public string SecondaryPosition { get; set; }
 
-        public string Race { get; set; }
+    public string Race { get; set; }
 
-        public string Religion { get; set; }
+    public string Religion { get; set; }
 
-        public string PoliticalAlignment { get; set; }
+    public string PoliticalAlignment { get; set; }
 
-        public string Gender { get; set; }
+    public string Gender { get; set; }
 
-        public string Home { get; set; }
+    public string Home { get; set; }
 
-        public string Residence { get; set; }
+    public string Residence { get; set; }
 
-        public string[] Abilities { get; set; }
+    public string[] Abilities { get; set; }
 
-        public Dictionary<string, int> Skills { get; set; }
-        public bool Approved { get; set; }
+    public Dictionary<string, int> Skills { get; set; }
+    public bool Approved { get; set; }
 #nullable enable
-        public int? Age { get; set; }
+    public int? Age { get; set; }
 #nullable disable
 
-        public Character ()
-        {
-            FirstName = null;
-            MiddleName = null;
-            LastName = null;
-            PhysicalDescription = null;
-            Mother = null;
-            Father = null;
-            Children = null;
-            Siblings = null;
-            PersonalityAndTraits = null;
-        }
-
-        public Character (string firstName, string middleName, string lastName, string physicalDescription, string mother, string father, string children, string siblings, string personalityAndTraits, string weaknessesAndStrengths, string biography, Employment[] employmentHistory, CharacterAward[] awards, string generalHealth, string injuries, string surgeries, string illnesses, string mentalHealth, string modifications, string archetype, string primaryPosition, string secondaryPosition, string race, string religion, string politicalAlignment, string gender, string home, string residence, string[] abilities, Dictionary<string, int> skills, int age, bool approved)
-        {
-            FirstName = firstName;
-            MiddleName = middleName;
-            LastName = lastName;
-            PhysicalDescription = physicalDescription;
-            Mother = mother;
-            Father = father;
-            Children = children;
-            Siblings = siblings;
-            PersonalityAndTraits = personalityAndTraits;
-            WeaknessesAndStrengths = weaknessesAndStrengths;
-            Biography = biography;
-            EmploymentHistory = employmentHistory;
-            Awards = awards;
-            GeneralHealth = generalHealth;
-            Injuries = injuries;
-            Surgeries = surgeries;
-            Illnesses = illnesses;
-            MentalHealth = mentalHealth;
-            Modifications = modifications;
-            Archetype = archetype;
-            PrimaryPosition = primaryPosition;
-            SecondaryPosition = secondaryPosition;
-            Race = race;
-            Religion = religion;
-            PoliticalAlignment = politicalAlignment;
-            Gender = gender;
-            Home = home;
-            Residence = residence;
-            Abilities = abilities;
-            Skills = skills;
-            Age = age;
-            Approved = approved;
-        }
+    public Character()
+    {
+      FirstName = null;
+      MiddleName = null;
+      LastName = null;
+      PhysicalDescription = null;
+      Mother = null;
+      Father = null;
+      Children = null;
+      Siblings = null;
+      PersonalityAndTraits = null;
     }
+
+    public Character(string firstName, string middleName, string lastName, string physicalDescription, string mother, string father, string children, string siblings, string personalityAndTraits, string weaknessesAndStrengths, string biography, Employment[] employmentHistory, CharacterAward[] awards, string generalHealth, string injuries, string surgeries, string illnesses, string mentalHealth, string modifications, string archetype, string primaryPosition, string secondaryPosition, string race, string religion, string politicalAlignment, string gender, string home, string residence, string[] abilities, Dictionary<string, int> skills, int age, bool approved)
+    {
+      FirstName = firstName;
+      MiddleName = middleName;
+      LastName = lastName;
+      PhysicalDescription = physicalDescription;
+      Mother = mother;
+      Father = father;
+      Children = children;
+      Siblings = siblings;
+      PersonalityAndTraits = personalityAndTraits;
+      WeaknessesAndStrengths = weaknessesAndStrengths;
+      Biography = biography;
+      EmploymentHistory = employmentHistory;
+      Awards = awards;
+      GeneralHealth = generalHealth;
+      Injuries = injuries;
+      Surgeries = surgeries;
+      Illnesses = illnesses;
+      MentalHealth = mentalHealth;
+      Modifications = modifications;
+      Archetype = archetype;
+      PrimaryPosition = primaryPosition;
+      SecondaryPosition = secondaryPosition;
+      Race = race;
+      Religion = religion;
+      PoliticalAlignment = politicalAlignment;
+      Gender = gender;
+      Home = home;
+      Residence = residence;
+      Abilities = abilities;
+      Skills = skills;
+      Age = age;
+      Approved = approved;
+    }
+  }
 }
--- Data/Entities/User.cs
+++ Data/Entities/User.cs
@@ -1,173 +1,172 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Collections.ObjectModel;
-using D20.Data.Entities.Characters;
+﻿using D20.Data.Entities.Characters;
 using D20.Data.Forms.Validations;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace D20.Data.Entities.Users
 {
-    interface IUser
+  interface IUser
+  {
+    string RealName { get; set; }
+    string Username { get; set; }
+    string Email { get; set; }
+    int Age { get; set; }
+    Collection<Character> Characters { get; set; }
+    DateTime Created { get; }
+    Moderator CreateModerator(string moderatorName);
+  }
+
+  public class User : IUser
+  {
+    private string _realName;
+    private string _username;
+    private string _email;
+    private int _age;
+    private Collection<Character> _characters = new();
+
+    private readonly DateTime _created = DateTime.Now;
+
+    [Required]
+    [StringLength(1000, ErrorMessage = "Let's not fool our selves. There's no way your character is this long!")]
+    [MaxLength(1000)]
+    public string RealName
     {
-        string RealName { get; set; }
-        string Username { get; set; }
-        string Email { get; set; }
-        int Age { get; set; }
-        Collection<Character> Characters { get; set; }
-        DateTime Created { get; }
-        Moderator CreateModerator(string moderatorName);
+      get
+      {
+        return _realName;
+      }
+      set
+      {
+        _realName = value;
+      }
     }
 
-    public class User : IUser
+    [Required]
+    [Username]
+    [StringLength(25, MinimumLength = 5, ErrorMessage = "Username must be 5 to 25 characters long.")]
+    [MinLength(5)]
+    [MaxLength(25)]
+    public string Username
     {
-        private string _realName;
-        private string _username;
-        private string _email;
-        private int _age;
-        private Collection<Character> _characters = new();
-
-        private readonly DateTime _created = DateTime.Now;
-
-        [Required]
-        [FullName]
-        [StringLength(1000, ErrorMessage = "Let's not fool our selves. There's no way your character is this long!")]
-        [MaxLength(1000)]
-        public string RealName
-        {
-            get
-            {
-                return _realName;
-            }
-            set
-            {
-                _realName = value;
-            }
-        }
-
-        [Required]
-        [Username]
-        [StringLength(25, MinimumLength = 5, ErrorMessage = "Username must be 5 to 25 characters long.")]
-        [MinLength(5)]
-        [MaxLength(25)]
-        public string Username
-        {
-            get
-            {
-                return _username;
-            }
-            set
-            {
-                _username = value;
-            }
-        }
-
-        [Required]
-        [EmailAddress]
-        public string Email
-        {
-            get
-            {
-                return _email;
-            }
-            set
-            {
-                _email = value;
-            }
-        }
-
-        [Required]
-        [Range(18, 200, ErrorMessage = "You must be at least 18 to play.")]
-        public int Age
-        {
-            get
-            {
-                return _age;
-            }
-            set
-            {
-                _age = value;
-            }
-        }
-
-        public Collection<Character> Characters
-        {
-            get
-            {
-                return _characters;
-            }
-            set
-            {
-                _characters = value;
-            }
-        }
-
-        public DateTime Created
-        {
-            get
-            {
-                return _created;
-            }
-        }
-
-        public User(string realName, string username, string email, int age)
-        {
-            RealName = realName;
-            Username = username;
-            Email = email;
-            Age = age;
-        }
-
-        public Moderator CreateModerator(string moderatorName)
-        {
-            return new Moderator(_realName, _username, _email, _age, moderatorName);
-        }
+      get
+      {
+        return _username;
+      }
+      set
+      {
+        _username = value;
+      }
     }
 
-    interface IModerator
-    { 
-        string ModeratorName { get; set; }
-        Game[] Games { get; set; }
-    }
-
-    /// <summary>
-    /// A moderator moderates a game in whatever way a game master sees fit. A moderator's identity is separate
-    /// from any character they may play, and will not be connected to any characters they have.
-    /// </summary>
-    public class Moderator : User, IModerator
+    [Required]
+    [EmailAddress]
+    public string Email
     {
-        private string _moderatorName;
-        private Game[] _games;
-
-        [Required]
-        [StringLength(25, MinimumLength = 5, ErrorMessage = "Name must be 5 to 25 characters.")]
-        [MinLength(5)]
-        [MaxLength(25)]
-        public string ModeratorName
-        {
-            get
-            {
-                return _moderatorName;
-            }
-            set
-            {
-                _moderatorName = value;
-            }
-        }
-
-        [Required]
-        public Game[] Games
-        {
-            get
-            {
-                return _games;
-            }
-            set
-            {
-                _games = value;
-            }
-        }
-
-        public Moderator(string username, string realName, string email, int age, string moderatorName) : base(realName, username, email, age)
-        {
-            ModeratorName = moderatorName;
-        }
+      get
+      {
+        return _email;
+      }
+      set
+      {
+        _email = value;
+      }
     }
+
+    [Required]
+    [Range(18, 200, ErrorMessage = "You must be at least 18 to play.")]
+    public int Age
+    {
+      get
+      {
+        return _age;
+      }
+      set
+      {
+        _age = value;
+      }
+    }
+
+    public Collection<Character> Characters
+    {
+      get
+      {
+        return _characters;
+      }
+      set
+      {
+        _characters = value;
+      }
+    }
+
+    public DateTime Created
+    {
+      get
+      {
+        return _created;
+      }
+    }
+
+    public User(string realName, string username, string email, int age)
+    {
+      RealName = realName;
+      Username = username;
+      Email = email;
+      Age = age;
+    }
+
+    public Moderator CreateModerator(string moderatorName)
+    {
+      return new Moderator(_realName, _username, _email, _age, moderatorName);
+    }
+  }
+
+  interface IModerator
+  {
+    string ModeratorName { get; set; }
+    Game[] Games { get; set; }
+  }
+
+  /// <summary>
+  /// A moderator moderates a game in whatever way a game master sees fit. A moderator's identity is separate
+  /// from any character they may play, and will not be connected to any characters they have.
+  /// </summary>
+  public class Moderator : User, IModerator
+  {
+    private string _moderatorName;
+    private Game[] _games;
+
+    [Required]
+    [StringLength(25, MinimumLength = 5, ErrorMessage = "Name must be 5 to 25 characters.")]
+    [MinLength(5)]
+    [MaxLength(25)]
+    public string ModeratorName
+    {
+      get
+      {
+        return _moderatorName;
+      }
+      set
+      {
+        _moderatorName = value;
+      }
+    }
+
+    [Required]
+    public Game[] Games
+    {
+      get
+      {
+        return _games;
+      }
+      set
+      {
+        _games = value;
+      }
+    }
+
+    public Moderator(string username, string realName, string email, int age, string moderatorName) : base(realName, username, email, age)
+    {
+      ModeratorName = moderatorName;
+    }
+  }
 }
